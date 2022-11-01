1cbe8eb5bb88e1b4f6a8a82bcbc8b57c8df0a878
Prathmesh Chaudhari <76175584+prathmeshkc@users.noreply.github.com>
2022-10-31 20:26:24 -0400

    Time: 8 ms (52.42%), Space: 47.8 MB (37.87%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,41 +1,46 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
         
-        HashSet<String> set = new HashSet<>();
+        HashMap<Integer, Integer>[] rows = new HashMap[9];
+        HashMap<Integer, Integer>[] cols = new HashMap[9];
+        HashMap<Integer, Integer>[] boxes =  new HashMap[9];
         
-        for(int row=0; row<9; row++){
-            for(int col=0; col<9;col++){
-                
-                if(board[row][col] != '.'){
-                    int boxIndex = (row/3)*3 + (col/3);
-                    String rowStr = "row" + row + board[row][col];
-                    String colStr = "col" + col + board[row][col];
-                    String boxStr = "box" + boxIndex + board[row][col];
-                    
-                    if(!set.add(rowStr)){
-                        return false;
-                    }
-                    
-                    if(!set.add(colStr)){
-                        return false;
-                    }
-                    
-                    if(!set.add(boxStr)){
-                        return false;
-                    }               
-                    
-                }
-                
-                
-                
-            }
+        
+        for(int i=0; i<9; i++){
+            // [{k:v, k1:v1,.....}, {},{},{}.......,{}]
+            rows[i] = new HashMap<Integer, Integer>();
+            cols[i] = new HashMap<Integer, Integer>();
+            boxes[i] = new HashMap<Integer, Integer>();
+            
         }
         
         
-        
-        
-        
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9;j++){
+                
+                if(board[i][j] != '.'){
+                    
+                    int element = (int)board[i][j];
+                    int boxIndex = (i/3)*3 + (j/3);
+                    
+                    rows[i].put(element, rows[i].getOrDefault(element,0) + 1);
+                    cols[j].put(element, cols[j].getOrDefault(element, 0)+1);
+                    boxes[boxIndex].put(element, boxes[boxIndex].getOrDefault(element,0)+1);
+                    
+                    if(rows[i].get(element) > 1 || cols[j].get(element) > 1 || boxes[boxIndex].get(element) > 1){
+                        return false;
+                    }
+                    
+                }             
+                
+                
+            }
+            
+            
+        }
         
         return true;
+        
+        
     }
 }
