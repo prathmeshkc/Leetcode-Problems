5a902b055d8ea77868c9b1cdc95190443feafa1d
Prathmesh Chaudhari <76175584+prathmeshkc@users.noreply.github.com>
2022-11-02 19:07:39 -0400

    Time: 857 ms (30.13%), Space: 381.5 MB (12.74%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        
+        
+        if(s.equals(" ")){
+            return true;
+        }
+        
+        
+        String plainText = "";
+        
+        for(char c: s.toCharArray()){
+            if(Character.isLetter(c) || Character.isDigit(c)){
+                plainText += c;
+            }
+        }
+        
+        plainText = plainText.toLowerCase();
+        
+        int start = 0;
+        int end = plainText.length()-1;
+        
+        while(start<=end){
+            if(plainText.charAt(start) == plainText.charAt(end)){
+                start++;
+                end--;
+            }else{
+                return false;
+            }
+            
+        }
+        
+        return true;
+        
+        
+        
+        
+    }
+}
