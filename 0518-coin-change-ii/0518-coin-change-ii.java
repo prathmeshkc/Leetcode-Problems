af0ebe1466e3a9ff01a9f65a7b6337eb496a2867
Prathmesh Chaudhari <76175584+prathmeshkc@users.noreply.github.com>
2024-02-04 00:43:45 -0500

    Time: 7 ms (54.18%), Space: 48.5 MB (8.40%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int rows = coins.length;
+        int cols = amount;
+        
+        int[][] dp = new int[rows+1][cols+1];
+        dp[0][0] = 1;
+        
+        for(int i=1; i<=rows; i++) {
+            for(int j=0; j<=cols; j++) {
+                if(j < coins[i-1]) {
+                    dp[i][j] = dp[i-1][j];
+                } else {
+                    dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
+                }
+            }
+        }
+        
+        return dp[rows][cols];
+    }
+}
