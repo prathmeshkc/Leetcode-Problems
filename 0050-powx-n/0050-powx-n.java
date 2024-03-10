48339661bfb65eebbdc461c7007d50a84245d636
Prathmesh Chaudhari <76175584+prathmeshkc@users.noreply.github.com>
2024-03-09 19:48:08 -0500

    Time: 0 ms (100.00%), Space: 43.1 MB (11.36%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,17 @@
+class Solution {
+    public double myPow(double x, int n) {
+        //base
+        if(n == 0) return 1.0;
+        //logic
+        double res = myPow(x, n/2);
+        if(n % 2 == 0) {
+            return res*res;
+        } else {
+            if(n > 0) {
+                return res*res*x;
+            } else {
+                return res*res*(1/x);
+            }
+        }
+    }
+}
