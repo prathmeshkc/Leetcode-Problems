d6487bfbc66d572c86b676d49977928074e6612a
Prathmesh Chaudhari <76175584+prathmeshkc@users.noreply.github.com>
2024-03-30 23:59:03 -0400

    Time: 5 ms (7.55%), Space: 57 MB (53.64%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,18 +1,31 @@
 class Solution {
+    
+    //Code to return the start and end indices of the max sum subarray
     public int maxSubArray(int[] nums) {
-        int max = Integer.MIN_VALUE;
-        int rSum = 0;
-        
-        for(int i: nums) {
-            rSum += i;
-            if(rSum > max) {
-                max = rSum;
+        int n = nums.length;
+        int max = nums[0];
+        int curSum = nums[0];
+
+        int start = 0; int end = 0;
+        int curStart = 0;
+
+        for(int i = 1; i<n; i++) {
+            curSum += nums[i];
+            
+            if(curSum < nums[i]) {
+                curSum = nums[i];
+                curStart = i;
             }
             
-            if(rSum < 0) {
-                rSum = 0;
+            if(curSum > max) {
+                max = curSum;
+                start = curStart;
+                end = i;
             }
         }
+        
+        System.out.println(start);
+        System.out.println(end);
         
         return max;
     }
