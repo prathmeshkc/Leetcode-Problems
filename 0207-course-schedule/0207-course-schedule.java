e55af9721446a38ef36a1b5796a6f460294cb77c
Prathmesh Chaudhari <76175584+prathmeshkc@users.noreply.github.com>
2024-05-09 15:45:43 -0400

    Time: 7 ms (35.71%), Space: 45.5 MB (33.88%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,48 +1,47 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int[] indeg = new int[numCourses];
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        int count = 0;
+        if(prerequisites.length == 0) return true;
+        Map<Integer, List<Integer>> deps = new HashMap<>();
+        int[] indegree = new int[numCourses];
         
+//         fill the indegree array and create the deps map
         for(int[] edge: prerequisites) {
             int dep = edge[0];
-            int indep = edge[1];
-            
-            indeg[dep] += 1;
-            
-            if(!map.containsKey(indep)) {
-                map.put(indep, new ArrayList<>());
+            int inDep = edge[1];
+            indegree[dep]++;
+            if(!deps.containsKey(inDep)) {
+                deps.put(inDep, new ArrayList<>());
             }
-            map.get(indep).add(dep);
-            
+            deps.get(inDep).add(dep);
         }
         
         Queue<Integer> q = new LinkedList<>();
-        
-        for(int i=0; i<numCourses; i++) {
-            if(indeg[i] == 0) {
+// count to check how many courses are we able to complete so we can tally with numCourses
+        int count = 0;
+        for(int i = 0; i<numCourses; i++) {
+            if(indegree[i] == 0) {
                 q.add(i);
                 count++;
             }
         }
         
-        if(q.isEmpty()) return false;
-        
         while(!q.isEmpty()) {
-            int cur = q.poll();
-            List<Integer> deps = map.get(cur);
-            if(deps == null) continue;
-            for(Integer dep: deps) {
-                indeg[dep] -= 1;
-                if(indeg[dep] == 0) {
+            int inDep = q.poll();
+            List<Integer> dependants = deps.get(inDep);
+            if(dependants == null) continue;
+            for(int dep: dependants) {
+                indegree[dep]--;
+                if(indegree[dep] == 0) {
                     q.add(dep);
                     count++;
                     if(count == numCourses) return true;
                 }
             }
         }
-        if(count == numCourses) return true;
+        
+        
         return false;
+        
         
     }
 }
