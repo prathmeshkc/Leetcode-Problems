464d6bce64d84261fc14e291aba05a2521190362
Prathmesh Chaudhari <76175584+prathmeshkc@users.noreply.github.com>
2024-05-19 17:19:23 -0400

    Time: 2 ms (84.91%), Space: 44.5 MB (44.01%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -6,25 +6,28 @@ class Solution {
         return res;
     }
     
-    private void helper(int[] candidates, int idx, List<Integer> path, int target) {
+    private void helper(int[] candidates, int pivot, List<Integer> path, int target) {
         //base
-        if(idx == candidates.length || target < 0) return;
-        
         if(target == 0) {
             res.add(new ArrayList<>(path));
             return;
         }
         
+        if(target < 0) return; //IMP condition. Otherwise, StackOverflow
         
-        ///logic
-                
-        //choose
-        path.add(candidates[idx]);
-        helper(candidates, idx, path, target - candidates[idx]);
-        //backtrack
-        path.remove(path.size()-1);
+        //for-loop based recursion
+        for(int i = pivot; i < candidates.length; i++) {
+            
+            //action
+            path.add(candidates[i]);
+            
+            //recurse
+            helper(candidates, i, path, target - candidates[i]);
+            
+            //backtrack
+            path.remove(path.size() - 1);
+            
+        }
         
-        //don't choose
-        helper(candidates, idx+1, path, target);
     }
 }
