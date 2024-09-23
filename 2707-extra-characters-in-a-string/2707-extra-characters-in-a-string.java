b1861e128561d9f6ceced3534accb9efe8999371
Prathmesh Chaudhari <76175584+prathmeshkc@users.noreply.github.com>
2024-09-23 13:11:13 -0400

    Time: 46 ms (42.18%), Space: 45.3 MB (54.42%) - LeetHub

---
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
@@ -0,0 +1,36 @@
+class Solution {
+    private Set<String> set;
+    private int[] mem;
+    public int minExtraChar(String s, String[] dictionary) {
+        this.set = new HashSet<>();
+        this.mem = new int[51];
+        Arrays.fill(mem, -1);
+        
+        for(String str: dictionary) {
+            set.add(str);
+        }
+        
+        return solve(0, s, s.length());
+    }
+    
+    private int solve(int idx, String s, int n) {
+        //base
+        if(idx >= n) return 0;
+        
+        //logic
+        //check if we've already computed
+        if(mem[idx] != -1) return mem[idx];
+            
+        //choose as extra char (meaning skip it in the substring)
+        int res = 1 + solve(idx + 1, s, n);
+        
+        //dont choose as extra char (meaning dont skip it in the substring)
+        for(int j = idx; j < n; j++) {
+            String curr = s.substring(idx, j+1);
+            if(set.contains(curr)) {
+                res = Math.min(res, solve(j+1, s, n));
+            }
+        }
+        return mem[idx] = res;
+    }
+}
