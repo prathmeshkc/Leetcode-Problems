4dd621f56e34937df4344dec2cb20e07dc66f111
Prathmesh Chaudhari <76175584+prathmeshkc@users.noreply.github.com>
2024-09-30 15:56:24 -0400

    Time: 5 ms (42.21%), Space: 55.5 MB (58.64%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.java
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.java
@@ -5,10 +5,13 @@ class Solution {
         
         if(original.length != m*n) return new int[0][0];
         
-        for(int i = 0; i < m*n; i++) {
-            int r = i / n;
-            int c = i % n;
-            res[r][c] = original[i];
+        int start = 0;
+        int end = n;
+        
+        for(int i = 0; i < m; i++) {
+            res[i] = Arrays.copyOfRange(original, start, end);
+            start = end;
+            end = end + n;
         }
         
         return res;
